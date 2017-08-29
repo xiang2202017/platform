6062c5ef04f9a4240be1b3b2fdfc85d1863d17b9
xiang2202017 <187373183@qq.com>
2017-08-29 18:58:48 +0800

    会员mapper、会员service、会员实体类

---
+++ HealthPlatn/src/main/java/com/health/entity/system/Member.java
@@ -0,0 +1,93 @@
+package com.health.entity.system;
+
+import java.util.Date;
+
+/**
+ * 会员实体
+ * @author XiangYu
+ *
+ */
+public class Member {
+	private Integer id;
+	private String memberNo;
+	private String memberName;
+	private Integer memberType;
+	private Integer sex;
+	private String idcardNo;
+	private String phone;
+	private String address;
+	private Date expireDate;
+	private String company;
+	private String password;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getMemberNo() {
+		return memberNo;
+	}
+	public void setMemberNo(String memberNo) {
+		this.memberNo = memberNo;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public Integer getMemberType() {
+		return memberType;
+	}
+	public void setMemberType(Integer memberType) {
+		this.memberType = memberType;
+	}
+	public Integer getSex() {
+		return sex;
+	}
+	public void setSex(Integer sex) {
+		this.sex = sex;
+	}
+	public String getIdcardNo() {
+		return idcardNo;
+	}
+	public void setIdcardNo(String idcardNo) {
+		this.idcardNo = idcardNo;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Date getExpireDate() {
+		return expireDate;
+	}
+	public void setExpireDate(Date expireDate) {
+		this.expireDate = expireDate;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
---
+++ HealthPlatn/src/main/java/com/health/service/system/MemberService.java
@@ -0,0 +1,59 @@
+package com.health.service.system;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.health.dao.system.DaoSupport;
+import com.health.entity.system.Page;
+import com.health.system.util.PageData;
+
+@Service("memberService")
+public class MemberService {
+
+	@Resource(name = "daoSupport")
+	private DaoSupport dao;
+	
+	//删除会员
+	public void deleteMemberById(Integer id) throws Exception{
+		dao.delete("MemberMapper.delete", id);			//删除会员
+	}
+	
+	//批量删除会员
+	public void deleteAllMembers(String[] ids) throws Exception{
+		dao.delete("MemberMapper.deleteAll", ids);
+	}
+	
+	//添加会员
+	public Integer insertMember(PageData pd) throws Exception{
+		return (Integer)dao.save("MemberMapper.save", pd);
+	}
+	
+	//根据id查找会员信息
+	public PageData getMemberById(String id) throws Exception{
+		return (PageData)dao.findForObject("MemberMapper.findById", id);
+	}
+	
+	//分页查询
+	@SuppressWarnings("unchecked")
+	public List<PageData> getDatalistPage(Page page) throws Exception {
+		return (List<PageData>)dao.findForList("MemberMapper.datalistPage", page);
+	}
+	
+	//编辑会员信息
+	public void updateMember(PageData pd) throws Exception{
+		dao.update("MemberMapper.update", pd);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<PageData> getMemberTypeList() throws Exception{
+		return (List<PageData>)dao.findForList("MemberTypeMapper.listAll","");
+	}
+	
+	public String getTypeName(Integer id) throws Exception{
+		return (String)dao.findForObject("MemberTypeMapper.getNameById", id);
+	}
+	
+}
